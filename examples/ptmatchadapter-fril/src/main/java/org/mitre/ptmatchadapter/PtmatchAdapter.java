b95304a87ea9420293a86da644ce318f102d1ba8
Michael Los <mel@mitr.org>
2016-03-03 07:29:53 -0500

    Delete commented out example reference to a sptring bean triggered by time

--- examples/ptmatchadapter-fril/src/main/java/org/mitre/ptmatchadapter/PtmatchAdapter.java
+++ examples/ptmatchadapter-fril/src/main/java/org/mitre/ptmatchadapter/PtmatchAdapter.java
@@ -16,9 +16,8 @@
  */
 package org.mitre.ptmatchadapter;
 
-import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
@@ -27,12 +26,6 @@ public class PtmatchAdapter extends FatJarRouter {
 
   @Override
   public void configure() {
-   // from("timer:trigger").transform().simple("ref:myBean").to("log:out");
   }
-
-//  @Bean
-//  String myBean() {
-//    return "I'm Spring bean!";
-//  }
 
 }
